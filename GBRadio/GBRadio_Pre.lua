be070f1642088a061e9c0130f26dd00bbbdaf089
Nasias <n@siasdarkstar.com>
2015-12-12 13:24:15 +0000

    Updated versioning and fixed version checker
    Version checker now actually works

--- GBRadio/GBRadio_Pre.lua
+++ GBRadio/GBRadio_Pre.lua
@@ -1,3 +1,3 @@
 GBRadio         = LibStub("AceAddon-3.0"):NewAddon("GBRadio", "AceComm-3.0", "AceSerializer-3.0", "AceEvent-3.0", "AceConsole-3.0");
 GBRMapFiles     = LibStub("LibMapData-1.0");
-GBRadio_VERSION = "v2.1";
+GBRadio_VERSION = "v2.1.1";
